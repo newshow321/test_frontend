ce638d395cc86545776774a87e0951a7111c2170
Azhar Ryad <azhar.ryad12@gmail.com>
2022-03-08 20:56:43 +0700

    inisial init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { Button } from 'reactstrap';
+// import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { Row, Input, Container, Col, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText } from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		// <Button color="danger">Danger!</Button>
+		<div>
+			<Navbar color="light" expand="md" light>
+				<NavbarBrand href="/">
+					Foods&Goods
+				</NavbarBrand>
+				<NavbarBrand href="/">
+					Icon
+				</NavbarBrand>
+			</Navbar>
+			<Container>
+				<Row>
+					<Col className="" xs="6">
+						Gambar 1
+					</Col>
+					<Col className="" xs="6">
+						Gambar 2
+					</Col>
+				</Row>
+			</Container>
+			<div>
+				<Container>
+					<Input placeholder="Cari Merchant atau barang ..." />
+				</Container>
+			</div>
+		</div>
+	);
 }
 
 export default App;
